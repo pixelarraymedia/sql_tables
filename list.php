b2a11d416be8ac3596b6295a80262998d0cb31ce
Darion <pixelarraymedia@gmail.com>
2022-06-12 15:52:45 -0700

    typo

--- list.php
+++ list.php
@@ -1,7 +1,8 @@
 <?php
 
 
-        /// 
+        /// Here we are sorting a list of arrays in alphabetical order
+        
         $list =  array("my","name","is","john","doe");
         sort($list);
 
